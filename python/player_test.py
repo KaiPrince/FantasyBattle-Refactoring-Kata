214193653a2f69b45f6e05fcf5986cadb24cf10e
Kai Prince <34746763+KaiPrince@users.noreply.github.com>
2023-03-25 21:50:55 -0700

    Split unit tests

--- python/player_test.py
+++ python/player_test.py
@@ -27,20 +27,37 @@ def player_calculate_damage_test():
     inventory = Inventory()
     stats = Stats(1)
     player = Player(inventory, equipment, stats)
-    armor = SimpleArmor(1)
-    buffs = [BasicBuff(1.0, 1.5)]
-    target = SimpleEnemy(armor, buffs)
 
     # Act
     damage = player.calculate_damage()
-    result = Damage.betweenTargets(player, target)
 
     # Assert
     print(damage)
     assert damage == 28
 
+
+def damage_between_targets_test():
+    # Arrange
+    left_hand = BaseItem("left hand", 1, 1.0)
+    right_hand = BaseItem("right hand", 1, 1.5)
+    head = BaseItem("head", 1, 1.0)
+    chest = BaseItem("chest", 1, 1.0)
+    feet = BaseItem("feet", 1, 1.0)
+    equipment = Equipment(left_hand, right_hand, head, chest, feet)
+    inventory = Inventory()
+    stats = Stats(1)
+    player = Player(inventory, equipment, stats)
+    armor = SimpleArmor(1)
+    buffs = [BasicBuff(1.0, 1.5)]
+    target = SimpleEnemy(armor, buffs)
+
+    # Act
+    result = Damage.betweenTargets(player, target)
+
+    # Assert
     print(result.amount)
     assert result.amount == 26
 
 
 player_calculate_damage_test()
+damage_between_targets_test()
